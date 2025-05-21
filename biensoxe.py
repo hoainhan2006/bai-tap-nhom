4c1f46193767ea79f49e1983bfa6a1880b82161a
hoainhan2006 <nhan4215789@gmail.com>
2025-05-21 18:29:34 +0700

    bản chỉnh sửa mới

--- biensoxe.py
+++ biensoxe.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import PhotoImage
 import cv2
+from tkinter import filedialog
 from PIL import Image, ImageTk
+import easyocr
 root=tk.Tk()
 root.title("Nhận diện biển số xe")
 root.geometry("800x400")
@@ -11,30 +13,32 @@ img=ImageTk.PhotoImage(img)
 img_label=tk.Label(root,image=img)
 img_label.place(x=0,y=0)
 tk.Label(root,text="Bài tập nhận diện biển số xe",bg="black",fg="white",font=("Times New Roman",22),relief=tk.GROOVE,bd=3,width=25).pack(pady=30)
-entry=tk.Entry(root,width=40)
-tk.Label(text="Hãy nhập đường dẫn hình ảnh vào đây").pack()
-baoloi2 = tk.Label(root)
-baoloi = tk.Label(root)
-baoloi3=tk.Label(root )
 img_label1=tk.Label(root)
-def anvao():
-    try:
-        global img1
-        img1=Image.open(entry.get().strip())
-        img1 = img1.resize((500, 450))
-        img1=ImageTk.PhotoImage(img1)
-        img_label1.config(image=img1)
-        img_label1.pack(padx=10,pady=10 )
-    except FileNotFoundError:
-        baoloi.pack(padx=10,pady=10)
-        baoloi.config(text='Không tìm thấy file')
-    except     IsADirectoryError:
-        baoloi3.config(text="Đây là thư mục ")
-        baoloi3.pack(padx=20,pady=20)
-    except AttributeError:
-        baoloi2.config(text="Bạn chưa nhập đường dẫn ")
-        baoloi2.pack()
-but=tk.Button(root,text="Nhận dạng biển số ",command=anvao)
-entry.pack()
-but.pack()
+text_label=tk.Label(root)
+bienso=' '
+def nut_tai_file():
+    filepath=filedialog.askopenfilename(initialdir="/home",title="Chọn file ảnh ",filetypes=( ("Tất cả ảnh", "*.jpg *.jpeg *.png *.bmp *.gif"), ("JPEG", "*.jpg *.jpeg"),("PNG", "*.png"),("Bitmap", "*.bmp"),("GIF", "*.gif")))
+    global img_tk,bienso
+    anh=cv2.imread(filepath)
+    gray_img=cv2.cvtColor(anh,cv2.COLOR_BGR2GRAY)
+    _,binary_img = cv2.threshold(gray_img, 128, 255, cv2.THRESH_BINARY_INV)
+    contours, hierarchy = cv2.findContours(binary_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if 50<w and 50<h<250:
+            anhcat=anh[x:x+w, y:y+h]
+            cv2.rectangle(anh, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(anh)
+    for bbox, text, conf in result:
+        bienso = bienso + text
+    img_pil = Image.fromarray(anh)
+    img_tk = img_pil.resize((500, 450))
+    img_tk = ImageTk.PhotoImage(img_tk)
+    img_label1.configure(image=img_tk)
+    img_label1.pack()
+    text_label.configure(text=bienso)
+    text_label.pack()
+button=tk.Button(root,text="Tải ảnh",command=nut_tai_file)
+button.pack()
 root.mainloop()
