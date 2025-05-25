59300177237c62c690bff4d87c76e706d76424ff
hoainhan2006 <nhan4215789@gmail.com>
2025-05-25 22:30:25 +0700

    chỉnh sửa nhìn cho gọn

--- biensoxe.py
+++ biensoxe.py
@@ -13,22 +13,22 @@ img=ImageTk.PhotoImage(img)
 img_label=tk.Label(root,image=img)
 img_label.place(x=0,y=0)
 tk.Label(root,text="Bài tập nhận diện biển số xe",bg="black",fg="white",font=("Times New Roman",22),relief=tk.GROOVE,bd=3,width=25).pack(pady=30)
-img_label1=tk.Label(root)
-text_label=tk.Label(root)
 def nut_tai_file():
     filepath=filedialog.askopenfilename(initialdir="/home",title="Chọn file ảnh ",filetypes=( ("Tất cả ảnh", "*.jpg *.jpeg *.png *.bmp *.gif"), ("JPEG", "*.jpg *.jpeg"),("PNG", "*.png"),("Bitmap", "*.bmp"),("GIF", "*.gif")))
     global img_tk
-    bienso=" "
     anh=cv2.imread(filepath)
     reader = easyocr.Reader(['en'])
     result = reader.readtext(anh)
+    bienso = " "
     for bbox, text, conf in result:
             bienso = bienso + text + "\n"
     img_pil = Image.fromarray(anh)
     img_tk = img_pil.resize((500,400 ))
     img_tk = ImageTk.PhotoImage(img_tk)
+    img_label1 = tk.Label(root)
     img_label1.configure(image=img_tk)
     img_label1.pack(pady=30)
+    text_label = tk.Label(root)
     text_label.configure(text=bienso.strip(),font=("Times New Roman ",36))
     text_label.pack()
 button=tk.Button(root,text="Tải ảnh",command=nut_tai_file)
